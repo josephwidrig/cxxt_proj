571b10f71c7184b53319ec742ba8c349b5f617a6
josephwidrig <joseph.widrig@gmail.com>
2013-10-29 16:06:07 -0400

    10-29-13 changes

--- moveZipFile.ps1
+++ moveZipFile.ps1
@@ -1,31 +1,49 @@
-﻿[System.Reflection.Assembly]::LoadWithPartialName('System.IO.Compression.FileSystem')
+﻿#Joseph Widrig and David McPherson
+#
+#TITLE
+#[System.Reflection.Assembly]::LoadWithPartialName('System.IO.Compression.FileSystem')
 
 Function moveZipFile()
 {
-    Move-Item C:\Users\Joseph\Downloads\cxxtest-4.3.zip C:\Users\Joseph\cxxt_proj\CxxTest
+    #eventually make this version-flexible
+    if( (Test-Path ~\Downloads\cxxtest-4.3.zip -PathType leaf) )
+    {
+    Move-Item ~\Downloads\cxxtest-4.3.zip ~\cxxt_proj\CxxTest
+    }
+    else
+    {
+        Write-Error "cxxtest-4.3.zip not found"   
+    }
 }
 
 
 #TO ADD: ask whether CxxTest dir exists and then decide whether to do this function
 Function createUnzipDirectory()
 {
+    cd ~
+    if(!(Test-Path .\cxxt_proj -PathType Container) ) #test to see if cxxt_proj already exists
+    {
+        mkdir cxxt_proj
+    }
     #move to working directory (can be changed)
-    cd C:\Users\Joseph\cxxt_proj
-    dir
-    mkdir CxxTest
+    cd ~\cxxt_proj
+    if(!(Test-Path .\CxxTest -PathType Container) )
+    {
+        mkdir CxxTest
+    }
     #[System.IO.Compression.ZipFile]::ExtractToDirectory('~\cxxt_proj\cxxtest-4.3.zip', '~\cxxt_proj\CxxTest')
 }
 
 
-
-Function maybeUnzip()
+#changed from maybeUnzip
+Function unzip()
 {
     ls
     #unzips all compressed files in the directory
-    $shell=new-object -com shell.application
+    $shell=new-object -com shell.application #gets a shell object
     $CurrentLocation=get-location
     #target destination
-    cd C:\Users\Joseph\cxxt_proj\CxxTest
+    cd ~\cxxt_proj\CxxTest
     $cxxtestdir=Get-Location
     #changed '=$currentLocation.path' to '$cxxtestdir'
     $CurrentPath=$cxxtestdir.path
@@ -36,6 +54,7 @@ Function maybeUnzip()
     $ZipFolder=$cxxtestdir
     foreach ($ZipFile in $ZipFiles)
     {
+        echo "unzipping:"
         $ZipFile.fullname | out-default
         $ZipFolder = $shell.namespace($ZipFile.fullname)
         #error returned from this line
@@ -43,9 +62,16 @@ Function maybeUnzip()
         $location.Copyhere($ZipFolder.items())
         
     }
+    echo "The cxxtest files have been extracted to cxxtest 4.3"
 }
 
 createUnzipDirectory
 moveZipFile
-maybeUnzip
-#at this point all zip files have been extracted in the CxxTest folder
+unzip
+#at this point all zip files have been extracted in the CxxTest folder - 10/21/13
+
+#~\cxxt_proj\CxxTest\cxxtest-4.3   [\cxxtest] <--- must be moved to C:\Program Files (x86)\MSVS 11.0\VC\include
+#                                                                or C:\Program Files\Microsoft SDKs\Windows\v*.*\Include\cxxtest
+#  <using microsoft
+#  investigate cygwin and mingw
+# ADD IN echo(s) OR Write-(s)
